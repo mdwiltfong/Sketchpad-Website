44fc76d2188b0d9ecfdd90e114b518fd9a3d049d
Michael Wiltfong <wiltfong.michael1@gmail.com>
2023-05-14 11:08:27 -0400

    Eye dropper renders corerctly

--- src/components/Tools/EyeDropper.ts
+++ src/components/Tools/EyeDropper.ts
@@ -2,7 +2,7 @@ import Tool from "./Tool";
 export default class EyeDropper extends Tool {
   private parentDiv: HTMLDivElement;
   constructor(pathD: string) {
-    super("color-picker-menu", "eye-btn", pathD, "button", "icon", "eye-btn");
-    this.element.setAttribute("id", "eyedropper");
+    super("color-picker-menu", "eye-btn", pathD, "button", "icon", "eraser");
+    this.element.setAttribute("id", "eye-btn");
   }
 }
