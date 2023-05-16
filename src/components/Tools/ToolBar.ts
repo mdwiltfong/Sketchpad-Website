81f2ddb0f0992dfefbf2163804a9e5ea34308082
Michael Wiltfong <wiltfong.michael1@gmail.com>
2023-05-16 07:07:31 -0400

    Toolbar Class renders clear canvas and Save button

--- src/components/Tools/ToolBar.ts
+++ src/components/Tools/ToolBar.ts
@@ -1,10 +1,22 @@
 import Component, { insertAt } from "../BaseComponent";
 import Tool from "./Tool";
 export default class ToolBar extends Component<HTMLDivElement, HTMLDivElement> {
+  private clearCanvasBtn: HTMLButtonElement;
+  private saveCanvasBtn: HTMLButtonElement;
   constructor(...tools: Tool[]) {
     super("container", insertAt.beforeend, "toolbar");
     this.renderContent(...tools);
   }
-  public configure(): void {}
+  public configure(): void {
+    this.clearCanvasBtn = document.createElement("button");
+    this.clearCanvasBtn.innerText = "Clear Canvas";
+    this.clearCanvasBtn.id = "clear";
+    this.clearCanvasBtn.type = "button";
+
+    this.saveCanvasBtn = document.createElement("button");
+    this.saveCanvasBtn.innerText = "Save Canvas";
+    this.saveCanvasBtn.id = "save";
+    this.element.append(this.clearCanvasBtn, this.saveCanvasBtn);
+  }
   public renderContent(...tools: Tool[]): void {}
 }
